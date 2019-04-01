7a89c5d2b18f2e0d327c4211946d33bab9853b4d
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-04-01 10:44:24 +0300

    remove form component files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { RepoComponent } from './repo/repo.component';
-import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     RepoComponent,
-    FormComponent
+  
   ],
   imports: [
     BrowserModule
